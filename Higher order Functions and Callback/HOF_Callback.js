86b516b97216a3f2db614cd947aa77766fb05ad2
Naresh <120189841+nareshkumarme@users.noreply.github.com>
2023-07-11 23:46:46 +0530

    Add files via upload

---
+++ Higher order Functions and Callback/HOF_Callback.js
@@ -0,0 +1,104 @@
+
+// forEach() is HOF  and () ={} is callback function
+// const arr = ["hey","h1","hello","hola"];
+// arr.forEach((val) =>{
+//     // console.log(val)
+// })
+
+//Set Timeout
+
+// setTimeout(()=>{
+//     // console.log("NareshYella");
+// },3000);
+
+
+// setInterval(()=>{console.log("naresh")},1000);
+
+
+//map 
+
+// const numbers = [1,2,3,4,5];
+// const numSq = numbers.map((num) => num*num)
+// console.log(numSq)
+
+
+// filter
+
+// const count = ["reddy","kapu","kamma","yadava","velama"];
+// const results= count.filter((val)=> val.includes("dy"));
+// console.log(results)
+
+// reduce
+
+
+// const num = [1,2,3,4,5,6,7];
+// const sum = num.reduce((acc,curr)=>acc+curr,0);
+// console.log(sum)
+
+//DeStracturing
+
+// const Sci = [2.72,4.67,98.8,45,100];
+// let [e,pi,grav,bodytemp,boiltemp] = Sci;
+// console.log(e,pi,grav,bodytemp,boiltemp)
+
+// const arr1 = [1,2,3];
+// let [var1, ,var2] = arr1;
+// console.log(var1,var2)
+
+
+//rest
+
+// const arr2 = [1,2,3,4,5,6,7,8];
+// let [num1,num2,...rest] = arr2;
+// console.log(rest)
+
+
+//Spread
+
+// function sumone(x,y){
+//     return x + y;
+// }
+
+// let vari = [9,7];
+// console.log(sumone(...vari));
+
+
+// function sumall(...args){
+//     let sum1 = 0;
+//     console.log(args);
+//     for (let x of args){
+//         sum1 = x + sum1;
+
+
+//     }
+//     return sum1
+// }
+
+// console.log(sumall(1,2,3,4,5,6,7))
+
+
+
+
+//Try and Catch
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
